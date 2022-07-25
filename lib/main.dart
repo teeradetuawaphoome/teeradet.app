a9526e1cc7d3ec0ad7dba97bc5a5718c5f26c267
teeradetuawaphoome <108716921+teeradetuawaphoome@users.noreply.github.com>
2022-07-26 03:12:00 +0700

    tew6

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,53 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyAPP);
+  runApp(MyApp());
 }
 
+//สร้าง widget
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    MaterialApp(
+    return MaterialApp(
       title: "My App",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("สวัสดี ครับ"),
-        ),
-        body: Text("สวัสดีครับ ทุกคน"),
+      home: MyHomePage(),
+      theme: ThemeData(primarySwatch: Colors.red),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int number = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("กดเลย"),
       ),
-      theme: ThemeData(primarySwatch: Colors.green),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("กดปุ่มเพื่อเพิ่มจำนวนตัวเลข"),
+          Text(
+            "$number",
+            style: TextStyle(fontSize: 60),
+          ),
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text("กด"),
+      ),
     );
   }
 }
